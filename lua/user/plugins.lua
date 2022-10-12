0ddb3a51c60e453e9e2261fe8c43726de87df613
Hossein Khosravi <93550458+thehxdev@users.noreply.github.com>
2022-10-12 23:27:28 +0330

    Update plugins.lua

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -23,6 +23,7 @@ vim.cmd [[
   augroup end
 ]]
 
+local status_ok, packer = pcall(require, "packer")
 -- Use a protected call so we don't error out on first use local status_ok, packer = pcall(require, "packer")
 if not status_ok then
   return
