9842aabc3636d9d147bdc7823dba7ee1bf2dfae9
Hossein Khosravi <93550458+thehxdev@users.noreply.github.com>
2022-10-24 10:23:04 +0330

    Add files via upload

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -7,44 +7,10 @@
 -- gruvbox
 -- darkplus
 
-local colorscheme = "gruvbox"
+local colorscheme = "dracula"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
   vim.notify("colorscheme " .. colorscheme .. " not found!")
   return
 end
-
-
--- ######################## --
---   Neon Theme Options     --
--- ######################## --
-
---vim.g.neon_style = "default"  -- default , doom , dark
---vim.g.neon_italic_keyword = true
---vim.g.neon_italic_function = true
---vim.g.neon_transparent = false
-
--- ---------------------------------------------------------- --
-
--- ######################## --
---    Nord Theme Options    --
--- ######################## --
-
---vim.g.nord_contrast = true
---vim.g.nord_borders = false
---vim.g.nord_disable_background = false
---vim.g.nord_italic = true
---vim.g.nord_uniform_diff_background = true
-
--- ---------------------------------------------------------- --
-
--- ######################## --
---   Dracula Theme Options  --
--- ######################## --
-
---vim.g.show_end_of_buffer = true -- default false
---vim.g.transparent_bg = false -- default false
---vim.g.italic_comment = true -- default false
---vim.g.lualine_bg_color = "#44475a", -- default nil
-
--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -38,7 +38,7 @@ keymap("n", "<C-Right>", ":vertical resize -2<CR>", opts)
 -- Navigate buffers
 keymap("n", "<S-l>", ":bnext<CR>", opts)
 keymap("n", "<S-h>", ":bprevious<CR>", opts)
-keymap("n", "<S-n>", ":Bdelete<CR>", opts)
+keymap("n", "<S-m>", ":Bdelete<CR>", opts)
 
 -- Insert --
 -- Press jk fast to enter
