4d1dc556e73af0de3b50cf112cddfbd2c7b73940
Hossein Khosravi <93550458+thehxdev@users.noreply.github.com>
2022-10-31 12:17:58 +0330

    Add files via upload

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -6,8 +6,19 @@
 -- nord
 -- gruvbox
 -- darkplus
+-- aurora
+-- codemonkey
+-- ferrum
+-- lunar
+-- onedark
+-- onedarker
+-- onedarkest
+-- onenord
+-- spacedark
+-- system76
+-- tomorrow
 
-local colorscheme = "dracula"
+local colorscheme = "tomorrow"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
--- lua/user/options.lua
+++ lua/user/options.lua
@@ -33,6 +33,12 @@ vim.opt.scrolloff = 4                           -- is one of my fav
 vim.opt.sidescrolloff = 8
 vim.opt.guifont = "monospace:h14"               -- the font used in graphical neovim applications
 
+vim.g.transparent_background = false       -- transparent background(Default: false)
+vim.g.italic_comments = true               -- italic comments(Default: true)
+vim.g.italic_keywords = true               -- italic keywords(Default: true)
+vim.g.italic_functions = true              -- italic functions(Default: false)
+vim.g.italic_variables = true              -- italic variables(Default: false)
+
 vim.opt.shortmess:append "c"
 
 vim.cmd "set whichwrap+=<,>,[,],h,l"
