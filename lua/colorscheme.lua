da9c72a4aa71fd473ad193a2ff1f81067abfc0bc
thehxdev <hossein.khosravi.ce@gmail.com>
2025-07-07 17:32:21 +0330

    fixed bugs
    Signed-off-by: thehxdev <hossein.khosravi.ce@gmail.com>

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -1,8 +1,6 @@
--- local colorscheme = "gruberdarker"
 local colorscheme = "tokyonight"
 -- local colorscheme = "catppuccin"
 -- local colorscheme = "gruvbox"
--- local colorscheme = "gruber-darker"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
